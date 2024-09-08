42bc9a8311c7caaab9cd85326fc2670b9846a6da
Jeff Manes <anwrak@icloud.com>
2024-09-07 23:05:57 -0700

    testing

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-player_df = pd.read_csv('datafiles/People.csv', encoding='latin-1')
+#import people data from csv
+import_people = pd.read_csv('datafiles/People.csv', encoding='latin-1')
+import_generations = pd.read_csv('datafiles/custom/generation.csv', encoding='latin-1')
 
-print(player_df)
+#remove non-players, remove unneeded columns
+people_df = import_people[~import_people['playerID'].str.endswith('99')].drop(['ID', 'deathYear', 'deathMonth', 'deathDay', 'deathCountry', 'deathState', 'deathCity', 'nameGiven'], axis=1)
+print(people_df)
