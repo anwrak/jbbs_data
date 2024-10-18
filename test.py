866dbf50e040348e853a7055d01049adf8dbaf49
Jeff Manes <anwrak@icloud.com>
2024-10-17 22:08:20 -0700

    remove old stuff

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-#import people data from csv
-import_people = pd.read_csv('datafiles/People.csv', encoding='latin-1')
-import_generations = pd.read_csv('datafiles/custom/generation.csv', encoding='latin-1')
-
-#remove non-players, remove unneeded columns
-people_df = import_people[~import_people['playerID'].str.endswith('99')].drop(['ID', 'deathYear', 'deathMonth', 'deathDay', 'deathCountry', 'deathState', 'deathCity', 'nameGiven'], axis=1)
-print(people_df)
