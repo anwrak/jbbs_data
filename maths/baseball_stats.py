839bed71a5d43450e031d6652c5c26fad4ec227c
Jeff Manes <anwrak@icloud.com>
2025-03-07 23:17:14 -0700

    hitting stats work

--- maths/baseball_stats.py
+++ maths/baseball_stats.py
@@ -1,180 +1,119 @@
+import numpy as np
+
 class BasicHitting:
     @staticmethod
+    #total bases
+    def calc_pa(df):
+        return df['AB'] + df['BB'] + df['HBP'] + df['SF'] + df['SH']
+    
+    @staticmethod
+    #singles
+    def calc_b1(df):
+        return df['H'] - df['B2'] - df['B3'] - df['HR']
+    
+    @staticmethod
+    #tatal bases
+    def calc_tb(df):
+        return BasicHitting.calc_b1(df) + (df['B2'] * 2) + (df['B3'] * 3) + (df['HR'] * 4)
+    
+    @staticmethod
+    #runs created
+    def calc_rp(df):
+        return df['R'] + df['RBI'] - df['HR']
+    
+    @staticmethod
+    #stolen base attempts
+    def calc_sba(df):
+        return df['SB'] + df['CS']
+    
+    @staticmethod
+    #extra base hits
+    def calc_xbh(df):
+        return df['B2'] + df['B3'] + df['HR']
+    
+    @staticmethod
+    #times on base
+    def calc_tob(df):
+        return df['H'] + df['BB'] + df['HBP']
+    
+    @staticmethod
+    #home runs per hit
+    def calc_hrh(df):
+        return np.where(df['H'] > 0, np.round(df['HR'] / df['H']), np.nan)
+    
+    @staticmethod
+    #home runs per at bat
+    def calc_hrab(df):
+        return np.where(df['AB'] > 0, np.round(df['HR'] / df['AB']), np.nan)
+
+    @staticmethod
+    #base runs
     def calc_br(df):
-        a = df["h"] + df["bb"] + df["hbp"] + (df["ibb"] * 0.5) - df["hr"]
-        b = ((1.4 * df["tb"]) - (0.6 * df["h"]) - (3 * df["hr"]) +
-             (0.1 * (df["bb"] + df["hbp"] - df["ibb"])) +
-             (0.9 * (df["sb"] - df["cs"] - df["gidp"]))) * 1.1
-        c = df["ab"] - df["h"] + df["cs"] + df["gidp"]
-        d = df["hr"]
-        return np.where(a + b > 0, np.round(((a * b) / (b + c)) + d, 3), np.nan)
+        A = df['H'] + df['BB'] + df['HBP'] + (df['IBB'] * 0.5) - df['HR']
+        B = ((1.4 * df['TB']) - (0.6 * df['H']) - (3 * df['HR']) + (0.1 * (df['BB'] + df['HBP'] - df['IBB']))) + (0.9 * (df['SB'] - df['CS'] - df['GIDP'])) * 1.1
+        C = df['AB'] - df['H'] + df['CS'] + df['GIDP']
+        D = df['HR']
+        return np.where(A + B > 0, np.round(((A * B) / (B + C)) + D, 3), np.nan)
+    
+    @staticmethod
+    #batting average on balls in play
+    def calc_babip(df):
+        A = df['H'] - df['HR']
+        B = df['AB'] - df['SO'] - df['HR'] + df['SF']
+        return np.where(B > 0, np.round(A / B, 3), np.nan)
+    
+    @staticmethod
+    #equivalent average
+    def calc_eqa(df):
+        A = df['H'] + BasicHitting.calc_tb(df) + (1.5 * (df['BB'] + df['HBP'])) + df['SB'] + df['SH'] + df['SF']
+        B = df['AB'] + df['BB'] + df['HBP'] + df['SH'] + df['SF'] + df['CS'] + (df['SB'] / 3)
+        return np.where(B > 0, np.round(A / B, 3), np.nan)
 
     @staticmethod
+    #batting average
     def calc_ba(df):
-        return np.where(df["ab"] > 0, np.round(df["h"] / df["ab"], 3), np.nan)
+        return np.where(df['AB'] > 0, np.round(df['H'] / df['AB'], 3), np.nan)
 
     @staticmethod
+    #on base percentage
     def calc_obp(df):
-        return np.where(df["pa"] > 0, np.round((df["h"] + df["bb"] + df["hbp"]) / df["pa"], 3), np.nan)
+        return np.where(df['PA'] > 0, np.round((df['H'] + df['BB'] + df['HBP']) / df['PA'], 3), np.nan)
 
     @staticmethod
+    #slugging percentage
     def calc_slg(df):
-        return np.where(df["ab"] > 0, np.round(df["tb"] / df["ab"], 3), np.nan)
+        return np.where(df['AB'] > 0, np.round(df['TB'] / df['AB'], 3), np.nan)
 
     @staticmethod
+    #on base plus slugging
     def calc_ops(df):
         return BasicHitting.calc_obp(df) + BasicHitting.calc_slg(df)
 
     @staticmethod
+    #isolated power
     def calc_iso(df):
         return BasicHitting.calc_slg(df) - BasicHitting.calc_ba(df)
 
     @staticmethod
-    def calc_gpa(df):
-        return np.where(BasicHitting.calc_slg(df) > 0,
-                        ((BasicHitting.calc_obp(df) * 1.8) + BasicHitting.calc_slg(df)) / 4,
-                        np.nan)
-
-    @staticmethod
-    def calc_rc(df):
-        a = df["h"] + df["bb"] - df["cs"] + df["hbp"] - df["gidp"]
-        b = (1.25 * df["b1"]) + (1.69 * df["b2"]) + (3.02 * df["b3"]) + (3.73 * df["hr"]) + \
-            (0.29 * (df["bb"] - df["ibb"] + df["hbp"])) + \
-            (0.492 * (df["sh"] + df["sf"] + df["sb"])) - (0.04 * df["k"])
-        c = df["pa"] * 9
-        top = ((2.4 * c) + a) * ((3 * c) + b)
-        btm = 9 * c
-        return np.where(btm > 0, (top / btm) * (0.9 * c), 0)
-
-    @staticmethod
-    def calc_sbp(df):
-        return np.where(df["sba"] > 0, df["sb"] / df["sba"], np.nan)
-
-    @staticmethod
-    def calc_ta(df):
-        t = df["tb"] + df["hbp"] + df["bb"] + df["sb"]
-        b = df["ab"] - df["h"] + df["cs"] + df["gidp"]
-        return np.where(b > 0, t / b, np.nan)
-
-class basic_hitting_old:
-    def __init__(self, stats):
-        #store provided stats 
-        self.g = stats['g']
-        self.ab = stats['ab']
-        self.r = stats['r']
-        self.h = stats['h']
-        self.b2 = stats['b2']
-        self.b3 = stats['b3']
-        self.hr = stats['hr']
-        self.rbi = stats['rbi']
-        self.sb = stats['sb']
-        self.cs = stats['cs']
-        self.bb = stats['bb']
-        self.k = stats['k']
-        self.ibb = stats['ibb']
-        self.hbp = stats['hbp']
-        self.sh = stats['sh']
-        self.sf = stats['sf']
-        self.gidp = stats['gidp']
-        #calculate the easy ones that don't require division
-        self.pa = stats['ab'] + stats['bb'] + stats['hbp'] + stats['sf'] + stats['sh']
-        self.rp = stats['r'] + stats['rbi'] - stats['hr']
-        self.b1 = stats['h'] - stats['b2'] - stats['b3'] - stats['hr']
-        self.sba = stats['sb'] + stats['cs']
-        self.tob = stats['h'] + stats['bb'] + stats['hbp']
-        self.tb = self.b1 + (stats['b2'] * 2) + (stats['b3'] * 3) + (stats['hr'] * 4)
-    
-    #base runs
-    def calc_br(self):
-        if self.ab > 0:
-            a = self.h = self.bb + self.hbp + (self.ibb * .5) - self.hr
-            b = ((1.4 * self.tb) - (.6 * self.h) - ( 3 * self.hr) + (.1 * (self.bb + self.hbp - self.ibb)) + (.9 * (self.sb - self.cs - self.gidp))) * 1.1
-            c = self.ab - self.h + self.cs + self.gidp
-            d = self.hr
-            return round(((a * b) / (b + c)) + d, 3) if a+b > 0 else None
-        else:
-            return None
-
-    #batting average  
-    def calc_ba(self):
-        return round(self.h / self.ab, 3) if self.ab > 0 else None
-    
-    #batting average on balls in play
-    def calc_babip(self):
-        if self.ab > 0:
-            t = self.h - self.hr
-            b = self.ab - self.k - self.hr + self.sf
-            return round(t/b, 3) if b>0 else 0
-        else:
-            return None
-    
-    #walks per strikeout
-    def calc_bbk(self):
-        return self.bb / self.k if self.k > 0 else self.bb
-    
-    #equivalent average
-    def calc_eqa(self):
-        t = self.h + self.tb + (1.5 * (self.bb + self.hbp)) + self.sb + self.sh + self.sf
-        b = self.ab + self.bb + self.hbp + self.sh + self.sf + self.cs + (self.sb / 3)
-        return t/b if b>0 else None
-
-    #on base percentage
-    def calc_obp(self):
-        return round((self.h + self.bb + self.hbp) / self.pa, 3) if self.pa > 0 else None
-   
-    #slugging percentage
-    def calc_slg(self):
-        return round(self.tb / self.ab, 3) if self.ab > 0 else None
-    
     #gross production average
-    def calc_gpa(self):
-        return ((self.calc_obp() * 1.8) + self.calc_slg()) / 4 if self.calc_slg() > 0 else None
+    def calc_gpa(df):
+        return np.where(BasicHitting.calc_slg(df) > 0, ((BasicHitting.calc_obp(df) * 1.8) + BasicHitting.calc_slg(df)) / 4, np.nan)
 
-    #at bats per homerun
-    def calc_abhr(self):
-        return round(self.ab / self.hr, 2) if self.hr > 0 else self.ab
-    
-    #hits per homerun
-    def calc_hhr(self):
-        return round(self.h / self.hr, 2) if self.hr > 0 else self.h
-    
-    #isolated something
-    def clac_iso(self):
-        return self.calc_slg() - self.calc_ba()
-
-    #on base plus slugging
-    def calc_ops(self):
-        obp = self.calc_obp()
-        slg = self.calc_slg()
-        if obp is not None and slg is not None:
-            return obp + slg
-        else:
-            return None
-
-    #plate appearances per walks
-    def calc_paso(self):
-        return self.pa / self.k if self.k > 0 else self.pa
-
+    @staticmethod
     #runs created
-    def calc_rc(self):
-        a = self.h + self.bb - self.cs + self.hbp - self.gidp
-        b = (1.25 * self.b1) + (1.69 * self.b2) + (3.02 * self.b3) + (3.73 * self.hr) + (.29 * (self.bb - self.ibb + self.hbp)) + (.492 * (self.sh + self.sf + self.sb)) - (.04 * self.k)
-        c = self.pa * 9
+    def calc_rc(df):
+        TOP = (df['H'] + df['BB']) * BasicHitting.calc
+        BTM = 9 * C
+        return np.where(BTM > 0, (TOP / BTM) * (0.9 * C), 0)
 
-        top = ((2.4 * c) + a) * ((3 * c) + b)
-        btm = 9*c
-
-        return (top/btm) * (.9 * c) if btm>0 else 0
-
+    @staticmethod
     #stolen base percentage
-    def calc_sbp(self):
-        return self.sb / self.sba if self.sba > 0 else None
-    
+    def calc_sbp(df):
+        return np.where(df['SBA'] > 0, df['SB'] / df['SBA'], np.nan)
+
+    @staticmethod
     #total average
-    def calc_ta(self):
-        t = self.tb + self.hbp + self.bb + self.sb
-        b = self.ab - self.h + self.cs + self.gidp
-        return t/b if b>0 else None
-
-
-    
+    def calc_ta(df):
+        T = df['TB'] + df['HBP'] + df['BB'] + df['SB']
+        B = df['AB'] - df['H'] + df['CS'] + df['GIDP']
+        return np.where(B > 0, T / B, np.nan)
